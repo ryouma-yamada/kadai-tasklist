1066e8daef24397e9f7553aa9c5cae09393a6462
ryouma.yamada <r10y20freedom@gmail.com>
2018-08-12 12:58:38 +0000

    add user_login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
-  root to: 'toppages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: 'tasks#index'
+  root to: 'tasks#index'
   
- resources :tasks
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  resources :users, only: [:index, :show, :new, :create]
+  resources :tasks
 end
