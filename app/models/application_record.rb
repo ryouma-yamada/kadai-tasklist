464b11dbb828ca0cfefaea3e8e1784948147889b
ryouma.yamada <r10y20freedom@gmail.com>
2018-08-15 07:50:07 +0000

    user new

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
-  belongs_to :user
+  #belongs_to :user
   self.abstract_class = true
-  
-  validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 255 }
 end
